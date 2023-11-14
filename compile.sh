ae9821c8e8f161a6783c0c07c97ea83f09406187
nzimm012 <nzimm012@ucr.edu>
2023-11-13 22:17:21 -0800

    Produce intermediate mil code for phase 3 part 1

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@ flex csplat.lex
 bison -v -d --file-prefix=y csplat.y
 gcc -O3 lex.yy.c -o parser.elf
 ./parser.elf input.txt > output.txt
-rm lex.yy.c y.output y.tab.c y.tab.h parser.elf
+./mil_run output.txt
